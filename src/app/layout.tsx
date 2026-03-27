72f70fb7c19f9d8e68f9b749696ad7990417d50e
Wanda <wanda.devops@gmail.com>
2026-03-27 23:59:44 +0100

    🔧 Sentinel: Add raw.githubusercontent.com to CSP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://formspree.io https://raw.githack.com;" />
+        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://formspree.io https://raw.githack.com https://raw.githubusercontent.com;" />
       </head>
       <body className="bg-brand-black text-brand-white antialiased">
         <a
